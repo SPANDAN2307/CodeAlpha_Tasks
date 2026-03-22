2f018c4afd66d6077b6ddc12e534d9cf52fb0de3
SPANDAN2307 <spandanchaudhari99@gmail.com>
2026-03-22 19:40:58 +0530

    Cleaned up nodejs modules

--- CodeAlpha_Backend_Development_Tasks/CodeAlpha_URL_shortner/run_server.bat
+++ CodeAlpha_Backend_Development_Tasks/CodeAlpha_URL_shortner/run_server.bat
@@ -1,8 +1,20 @@
 @echo off
+:: Change to the directory of the batch file so it runs correctly even as Administrator
+cd /d "%~dp0"
+
 echo Starting URL Shortener Setup...
 echo.
 
-if not exist "node_modules\" (
+:: Check for Node.js
+node -v >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [ERROR] Node.js is not installed or not in your PATH.
+    echo Please download and install Node.js from https://nodejs.org/
+    pause
+    exit /b
+)
+
+if not exist "node_modules" (
     echo [1/3] node_modules not found. Initializing package.json...
     if not exist "package.json" (
         call npm init -y > nul
